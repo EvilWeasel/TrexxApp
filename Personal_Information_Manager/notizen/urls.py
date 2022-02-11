46e8c7f8d85d212e83c2f53bf70bbeab7ba3ec5a
DroN <87648983+andnagel@users.noreply.github.com>
2022-02-11 10:06:54 +0100

    routing angepasst

--- Personal_Information_Manager/notizen/urls.py
+++ Personal_Information_Manager/notizen/urls.py
@@ -1,7 +1,8 @@
-from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('login', views.login, name='login'),
 ]
