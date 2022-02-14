4897ea1112e1c4ef4d02f95904c81997a056b827
DerDoktorAB <87469264+DerDoktorAB@users.noreply.github.com>
2022-02-14 14:15:50 +0100

    file upload schaltfläche

--- Personal_Information_Manager/notizen/urls.py
+++ Personal_Information_Manager/notizen/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('user/', views.user, name='user'),
     path('user/create/', views.userCreate, name='userCreate'),
     path('user/create/post', posts.userCreate, name='userCreate_post'),
+    path('user/file_upload', views.userUpload, name='userUpload' )
 ]
